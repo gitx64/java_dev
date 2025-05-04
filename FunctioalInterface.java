75ab0fe4c650d828e0bb59c917e72e743cb09d8b
gitx64 <gitify4002@gmail.com>
2025-05-04 06:03:46 -0700

    done

--- FunctioalInterface.java
+++ FunctioalInterface.java
@@ -18,7 +18,12 @@ public class FunctioalInterface{
 
         A obj = (int i) -> System.out.println("In integer show");
            
-         obj.show(0);   
+         obj.show(0); 
+         
+        //in case of single integer value in lambda expression we also dont have to declare .
+        A obj1 = i -> System.out.println("In short");
+
+        obj1.show(0);
         
     }
 }
