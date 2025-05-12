81e8a1515b6f71eb11613cbcdc5e2a469d2ab28d
Astik Goswami <gitify4002@gmail.com>
2025-05-12 11:32:37 +0530

    done Collectionss

---
+++ Collectionss.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collectionss {
+    public static void main(String[] args) {
+        Collection<Integer> c = new ArrayList<>();
+
+        c.add(4);
+        c.add(7);
+        c.add(8);
+        c.add(6);
+        c.add(5); 
+
+        System.out.println(c);
+    }
+}
